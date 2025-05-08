b6a72c68e094fea01b09f31d3b7ee1b6b0ff42d2
mora <64327020+intellectualmora@users.noreply.github.com>
2025-05-08 17:53:25 +0800

    update

---
+++ Source/VC_Territory/Patch_Scenario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HarmonyLib;
+using Verse;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using RimWorld;
+using RimWorld.BaseGen;
+using UnityEngine;
+using RimWorld.Planet;
+using MapModeFramework;
+namespace VC_Territory
+{
+    [HarmonyPatch]
+    public static class Patch_Scenario
+    {
+        [HarmonyPatch(typeof(Scenario), "PreMapGenerate")]
+        [HarmonyPrefix]
+        public static void PreMapGenerate_Prefix()
+        {
+            if (Current.Game != null && Current.Game?.InitData != null)
+            {
+                if (Current.Game.InitData.QuickStarted)
+                {
+                    TerritoryManager.InfluenceInitize();
+                    Log.Message("开发者模式初始完毕:" + TerritoryManager.territories.Count);
+                    return;
+                }
+                return;
+            }
+            Log.Message("加载完毕:" + TerritoryManager.territories.Count);
+          
+        }
+
+    }
+}
+
---
+++ Source/VC_Territory/Patch_UIRoot_Entry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HarmonyLib;
+using Verse;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using RimWorld;
+using RimWorld.BaseGen;
+using UnityEngine;
+using RimWorld.Planet;
+using MapModeFramework;
+namespace VC_Territory
+{
+    [HarmonyPatch]
+    public static class Patch_UIRoot_Entry
+    {
+        [HarmonyPatch(typeof(UIRoot_Entry), "Init")]
+        [HarmonyPrefix]
+        public static void Init_Prefix()
+        {
+              Materials.Init(); // 安全执行图形资源初始化
+        }
+
+    }
+}
+
