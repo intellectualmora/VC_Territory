5a00788624cbb0838130ec30b3957ce4b18fa98a
zch0713 <120911598+ThumbOnAStick@users.noreply.github.com>
2025-05-11 10:58:41 +1000

    Imp: When threat reward quest is complete, add 0.1 influence to random asker faction settlement and -0.1 to enemy faction settlement

---
+++ Source/VC_Territory/QuestPart_ChangeInfluence.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VC_Territory
+{
+    class QuestPart_ChangeInfluence : QuestPart
+    {
+        private Faction _faction;
+        private float _influenceChange;
+
+        public QuestPart_ChangeInfluence(Faction faction, float influenceChange)
+        {
+            _faction = faction;
+            _influenceChange = influenceChange;
+        }
+
+        public override void Cleanup()
+        {
+            base.Cleanup();
+            if(this.quest.State != QuestState.EndedFailed)
+            InfluenceHelper.AddInfluenceToFaction(_faction, _influenceChange);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_References.Look(ref _faction, "faction");
+            Scribe_Values.Look(ref _influenceChange, "influenceChange", 0f);
+        }
+    }
+}
